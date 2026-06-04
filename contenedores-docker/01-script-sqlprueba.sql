f98ab310924519f6c00027cd51c1b62f10574f8c
José Luis Gallardo <gallardohj2@gmail.com>
2026-06-04 11:19:45 -0600

    Se agregaron instrucciones al archivo 01-script-sqlprueba.sql, y se iniciara la unidad 2 de modelado de base de datos

--- contenedores-docker/01-script-sqlprueba.sql
+++ contenedores-docker/01-script-sqlprueba.sql
@@ -65,3 +65,45 @@ SELECT
 	MONTH(fecha_naci), DAY(fecha_naci), 
 	(DATEDIFF(year, fecha_naci, GETDATE())) AS Edad
 FROM alumno; 
+
+
+-- Ejemplo en Mariadb
+
+
+# Crea una base de datos
+CREATE DATABASE prueba_atributos;
+
+# Utiliza la base de datos
+USE prueba_atributos;
+
+# Creación de tabla empleado
+CREATE TABLE empleado(
+	id int not null PRIMARY KEY, 
+    nombre varchar(50) NOT NULL, 
+    apellido1 varchar (15) NOT NULL, 
+    apellido2 varchar (15) NULL, 
+    fecha_Ingreso DATE NOT NULL
+);
+
+INSERT INTO empleado
+VALUES (1, 'Reberiano', 'Robles', 'De la Torre', '1983-04-06');
+
+SELECT 
+	*, YEAR(fecha_ingreso), MONTH (fecha_ingreso), DAY(fecha_ingreso) 
+FROM empleado;
+
+ALTER TABLE empleado
+ADD COLUMN fecha_naci DATE NOT NULL;
+
+UPDATE empleado
+SET fecha_naci = '1983-04-06'
+WHERE id = 1;
+
+SELECT 
+	nombre, 
+    apellido1, 
+    apellido2, 
+    fecha_ingreso, 
+    fecha_naci, 
+    timestampdiff(year, fecha_naci, CURDATE()) AS Edad
+FROM empleado;
